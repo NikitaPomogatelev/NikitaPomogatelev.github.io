d81b6cc6351a52489d4a48def2735226820ff3da
Nikita Pomogatelev <nikitoshelper@mail.ru>
2020-03-27 23:35:11 +0500

    Вторая страница

--- designloft/src/js/main.js
+++ designloft/src/js/main.js
@@ -1,14 +1,20 @@
 $(function(){
     $('.slide-price').owlCarousel({
         margin: 30,
-        
+        dots: false,
         nav: false,
+        
         responsiveClass: true,
         responsive : {
             // breakpoint from 0 up
             0 : {
+                loop: true,
                 items: 1,
+                
+                dotsClass: 'price-dots',
+                dotClass: 'price-dot',
                 dots: true,
+                
                 
             },
             // breakpoint from 480 up
