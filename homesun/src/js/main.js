afa8c4156fa41c5b2a27d6676f8f9a2e69c31f88
Nikita Pomogatelev <nikitoshelper@mail.ru>
2020-04-03 23:52:42 +0500

    +2 секции + адаптив + настройка слайдера

--- homesun/src/js/main.js
+++ homesun/src/js/main.js
@@ -4,5 +4,37 @@ $(function() {
     margin: 10,
     nav:false,
     items: 1,
-})
+});
+
+$('.program-slider').owlCarousel({
+  loop:true,
+  margin: 10,
+  nav:false,
+  items: 1,
+});
+
+$('#persons-slider').owlCarousel({
+  loop:true,
+  margin: 10,
+  nav:false,
+  dotsClass: 'persons-dots',
+  dotClass: 'persons-dot',
+  dots: true,
+  items: 1,
+});
+
+$('#feedback-slider').owlCarousel({
+  loop:true,
+  margin: 10,
+  nav:true,
+  dotsClass: 'feedback-dots',
+  dotClass: 'feedback-dot',
+  navContainerClass: 'feedback-nav',
+  navClass: ['feedback-prev', 'feedback-next'],
+  navText: ['<img class="feedback__icon" src="img/feedback/arrow-left.svg">',
+  '<img class="feedback__icon" src="img/feedback/arrow-right.svg">'],
+  dots: true,
+  items: 1,
+
+});
 })
